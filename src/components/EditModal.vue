a6058e53011a5841d46f3398c6d9491d1d9dfeea
dot_ <muriloholiveira@hotmail.com>
2022-12-08 13:42:23 -0300

    :lipstick: Styling edit user modal

---
+++ src/components/EditModal.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+  import FormVue from './Form.vue'
+
+  import type { User } from '../types/User'
+  defineProps<{ user: User }>()
+</script>
+
+<template>
+  <section
+    class="relative top-[45%] left-[58%] -translate-x-[58%] -translate-y-[45%] w-3/5 h-3/5 bg-white p-8"
+  >
+    <section class="flex justify-between items-center">
+      <h1 class="text-xl font-bold pb-2">Editar usuário:</h1>
+      <button
+        class="text-red-500 font-medium text-sm p-1 hover:bg-red-500 hover:text-white"
+        @click="handleModal"
+      >
+        X Cancelar
+      </button>
+    </section>
+    <FormVue
+      :username="user.userName"
+      :fullname="user.name"
+      :email="user.email"
+    />
+    <button class="mt-5 base-btn w-full">Salvar Alterações</button>
+  </section>
+</template>
+
+<script lang="ts">
+  export default {
+    emits: ['close-modal'],
+    methods: {
+      handleModal() {
+        this.$emit('close-modal')
+      },
+    },
+  }
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,49 @@
+<template>
+  <form class="flex flex-col justify-between gap-y-5 w-full">
+    <section class="flex w-full gap-x-8">
+      <section class="flex flex-col w-full">
+        <label for="username" class="base-label">Login</label>
+        <input id="username" type="text" class="base-input" :value="username" />
+      </section>
+      <select></select>
+    </section>
+
+    <section class="flex w-full gap-x-8">
+      <section class="flex flex-col w-full">
+        <label for="fullName" class="base-label">Nome completo</label>
+        <input id="fullName" type="text" class="base-input" :value="fullname" />
+      </section>
+      <section class="flex flex-col w-full">
+        <label for="email" class="base-label">E-mail</label>
+        <input id="email" type="email" class="base-input" :value="email" />
+      </section>
+    </section>
+
+    <section class="flex w-full gap-x-8">
+      <section class="flex flex-col w-full">
+        <label for="password" class="base-label">Senha</label>
+        <input id="password" type="password" class="base-input" />
+      </section>
+      <section class="flex flex-col w-full">
+        <label for="repeatPassword" class="base-label"
+          >Confirmação de senha</label
+        >
+        <input id="repeatPassword" type="password" class="base-input" />
+      </section>
+    </section>
+  </form>
+</template>
+
+<script lang="ts">
+  export default {
+    props: {
+      username: { type: String, default: '' },
+      fullname: { type: String, default: '' },
+      email: { type: String, default: '' },
+    },
+    created() {},
+    methods: {
+      getUnits() {},
+    }
+  }
+</script>
